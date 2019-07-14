a688922bfd63322bd8f4e5742e6fa7a2974c497a
Mega <mohamedmagdy.9080@gmail.com>
2019-07-14 12:59:18 +0200

    Login Component Models

---
+++ TODO-MVC/Model/AuthenticationValidator.swift
@@ -0,0 +1,9 @@
+//
+//  AuthenticationValidator.swift
+//  TODO-MVC
+//
+//  Created by Mohamed El-Taweel on 7/14/19.
+//  Copyright © 2019 Demo. All rights reserved.
+//
+
+import Foundation
---
+++ TODO-MVC/Model/UserCredential.swift
@@ -0,0 +1,9 @@
+//
+//  UserCredential.swift
+//  TODO-MVC
+//
+//  Created by Mohamed El-Taweel on 7/14/19.
+//  Copyright © 2019 Demo. All rights reserved.
+//
+
+import Foundation
